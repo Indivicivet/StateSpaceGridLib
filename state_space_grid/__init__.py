3d9cda4b4b9766bed49101cce88f86b635aec707
Indivicivet <indivicivet@gmail.com>
2022-07-11 19:10:05 +0100

    add files with lowercase capitalization

---
+++ state_space_grid/__init__.py
@@ -0,0 +1,14 @@
+"""
+StateSpaceGridLib
+=================
+
+StateSpaceGridLib is a Python package for the creation of state space grids as defined in the paper
+'A new dynamic systems method for the analysis of early socioemotional development. Lewis, M. D., Lamey, A. V., & Douglas, L. (1999)'
+https://www.queensu.ca/psychology/sites/psycwww/files/uploaded_files/Faculty/Tom%20Hollenstein/GridWare/Lewis_Lamey__Douglas_1999.pdf
+"""
+
+__version__ = "0.0.1"
+
+
+from StateSpaceGrid.Grid import Grid, Gridstyle
+from StateSpaceGrid.Trajectory import Trajectory, Trajectorystyle
