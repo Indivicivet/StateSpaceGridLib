bf8b02dfa84a71379c2568421bc115e84b357884
Indivicivet <indivicivet@gmail.com>
2022-07-11 19:17:59 +0100

    add missing requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name="StateSpaceGrid",
-version="0.0.1",
-packages=find_packages(),
-install_requires=["networkx"])
+setup(
+    name="StateSpaceGrid",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "networkx",
+        "pandas",
+    ],
+)
