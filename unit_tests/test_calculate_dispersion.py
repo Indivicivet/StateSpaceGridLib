2c4c9e1c14e64687626a480074b6b852987b0431
Indivicivet <indivicivet@gmail.com>
2022-08-22 00:56:01 +0100

    parametrize calculate_dispersion() test by grid_points

--- unit_tests/test_calculate_dispersion.py
+++ unit_tests/test_calculate_dispersion.py
@@ -1,11 +1,12 @@
+import pytest
 from state_space_grid import grid, trajectory
 
-
-def test_calculate_dispersion_single_point():
+@pytest.mark.parametrize("grid_points", [2, 5])  # nb. 1 == div/0
+def test_calculate_dispersion_single_point(grid_points):
     traj = trajectory.Trajectory(
         data_x=[1],
         data_y=[1],
         data_t=[0, 1],
     )
-    result = grid.calculate_dispersion([traj], 5)
+    result = grid.calculate_dispersion([traj], grid_points)
     assert result == 0
