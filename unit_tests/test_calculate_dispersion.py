0c62628a306e257df7a48093f5e00b016e54ce92
Indivicivet <indivicivet@gmail.com>
2022-08-22 00:59:21 +0100

    add a two-point dispersion test

--- unit_tests/test_calculate_dispersion.py
+++ unit_tests/test_calculate_dispersion.py
@@ -13,3 +13,14 @@ def test_calculate_dispersion_single_point(x0, y0, t1, grid_points):
     )
     result = grid.calculate_dispersion([traj], grid_points)
     assert result == 0
+
+
+def test_calculate_dispersion_two_points():
+    traj = trajectory.Trajectory(
+        data_x=[1, 5],
+        data_y=[1, 5],
+        data_t=[0, 1, 3],
+    )
+    result = grid.calculate_dispersion([traj], 99)
+    expected = 0.50510204
+    assert abs(result - expected) < 1e-3, f"got {result}, expected about {expected}"
