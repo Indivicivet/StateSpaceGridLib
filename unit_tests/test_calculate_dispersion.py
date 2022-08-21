61ee693d1f8ca89ded7e321cfcb3f680ace17153
Indivicivet <indivicivet@gmail.com>
2022-08-22 00:56:51 +0100

    parametrize single point dispersion test over many values

--- unit_tests/test_calculate_dispersion.py
+++ unit_tests/test_calculate_dispersion.py
@@ -1,12 +1,15 @@
 import pytest
 from state_space_grid import grid, trajectory
 
+@pytest.mark.parametrize("x0", [0, 1, 3.7])
+@pytest.mark.parametrize("y0", [0, 1, 3.7])
+@pytest.mark.parametrize("t1", [1, 3.7])
 @pytest.mark.parametrize("grid_points", [2, 5])  # nb. 1 == div/0
-def test_calculate_dispersion_single_point(grid_points):
+def test_calculate_dispersion_single_point(x0, y0, t1, grid_points):
     traj = trajectory.Trajectory(
-        data_x=[1],
-        data_y=[1],
-        data_t=[0, 1],
+        data_x=[x0],
+        data_y=[y0],
+        data_t=[0, t1],
     )
     result = grid.calculate_dispersion([traj], grid_points)
     assert result == 0
