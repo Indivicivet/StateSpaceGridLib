c413bce248a70da705a627fae4abfa66a0a2c78e
Indivicivet <indivicivet@gmail.com>
2022-08-22 01:06:00 +0100

    move test_2 to test_end_to_end.py

---
+++ unit_tests/test_end_to_end.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import pandas as pd
+
+import state_space_grid as ssg
+
+
+DATA_PATH = Path(__file__).resolve().parent.parent / "examples" / "resources" / "ExampleData1.txt"
+
+
+# todo :: sensible name
+def test_2():
+    data1 = pd.read_csv(DATA_PATH.open())
+    traj1 = ssg.Trajectory(
+        data1["variable 1"].dropna().tolist(),
+        data1["variable 2"].dropna().tolist(),
+        data1["Onset"].dropna().tolist(),
+    )
+    # traj1.add_y_ordering(["Low", "Medium", "High"])
+    grid = ssg.Grid(
+        [traj1],
+        style=ssg.GridStyle(
+            title="test 2",
+            x_label="variable 1",
+            y_label ="variable 2",
+            title_font_size=30,
+            x_max=4,
+            x_min=1,
+            y_max=4,
+            y_min=1
+        ),
+    )
+    # grid.draw(
+    measures = grid.get_measures()
+    assert measures.mean_duration == 14.8
+    assert measures.mean_number_of_events == 8
+    assert measures.mean_number_of_visits == 7
+    assert measures.mean_cell_range == 6
+    assert measures.mean_duration_per_event == 1.85
+    assert abs(measures.mean_duration_per_visit - 2.1142857 < 1e-3)
+    assert abs(measures.mean_duration_per_cell - 2.46666 < 1e-3)
+    assert abs(measures.dispersion - 0.933333 < 1e-3)
+    assert measures.mean_missing_events == 0
+    assert measures.mean_missing_duration == 0
