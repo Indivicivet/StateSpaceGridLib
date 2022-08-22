0c0ea05185673dce02f5c0959de32888c3aa3bef
Indivicivet <indivicivet@gmail.com>
2022-08-22 01:49:15 +0100

    rename plot_example_data test_1 -> plot_example_data

--- examples/plot_example_data.py
+++ examples/plot_example_data.py
@@ -4,12 +4,9 @@ import pandas as pd
 
 from state_space_grid import grid, trajectory
 
-DATA_PATH = Path(__file__).resolve().parent / "resources" / "ExampleData1.txt"
 
-
-# todo :: sensible name
-def test_1():
-    data1 = pd.read_csv(DATA_PATH.open())
+def plot_example_data(data_path):
+    data1 = pd.read_csv(data_path.open())
     traj1 = trajectory.Trajectory(
         data1["variable 1"].dropna().tolist(),
         data1["variable 2"].dropna().tolist(),
@@ -23,4 +20,5 @@ def test_1():
 
 
 if __name__ == '__main__':
-    test_1()
+    DATA_PATH = Path(__file__).resolve().parent / "resources" / "ExampleData1.txt"
+    plot_example_data(DATA_PATH)
