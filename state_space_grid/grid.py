7152cda72bde06741602355355c354225ad37180
Indivicivet <indivicivet@gmail.com>
2022-08-22 01:27:54 +0100

    insert grid.process in the only place it's used

--- state_space_grid/grid.py
+++ state_space_grid/grid.py
@@ -62,6 +62,44 @@ class Grid:
         x_max = x_min
         y_max = y_min
         loops_list = []
+
+        def process(
+            grid_style: GridStyle,
+            trajectory: Trajectory,
+            max_duration: float,
+            x_min: int,
+            x_max: int,
+            y_min: int,
+            y_max: int,
+        ) -> tuple[float, int, int, int, int, set]:
+            """Get relevant data (and do merging of repeated states if desired)"""
+
+            x_data, y_data, t_data, loops = trajectory.get_states(grid_style.x_order, grid_style.y_order)
+
+            # Get min and max values
+            # todo :: this is probably silly...
+            temp_x_min, temp_x_max = util.calculate_min_max(x_data)
+            temp_y_min, temp_y_max = util.calculate_min_max(y_data)
+
+            if grid_style.x_min is not None:
+                x_min = grid_style.x_min
+            if grid_style.x_max is not None:
+                x_max = grid_style.x_max
+
+            if grid_style.y_min is not None:
+                y_min = grid_style.y_min
+            if grid_style.y_max is not None:
+                y_max = grid_style.y_max
+
+            return (
+                max(*(t2 - t1 for t1, t2 in zip(t_data, t_data[1:])), max_duration),
+                int(min(x_min, temp_x_min)),
+                int(min(y_min, temp_y_min)),
+                int(max(x_max, temp_x_max)),
+                int(max(y_max, temp_y_max)),
+                loops,
+            )
+
         for trajectory in self.trajectory_list:
             max_duration, x_min, y_min, x_max, y_max, loops = process(
                 self.style,
@@ -206,44 +244,6 @@ def calculate_extra_stuff(style, x_min, x_max, y_min, y_max):
             if y_max % cell_size_y
             else cell_size_y
         ),
-    )
-
-
-def process(
-    grid_style: GridStyle,
-    trajectory: Trajectory,
-    max_duration: float,
-    x_min: int,
-    x_max: int,
-    y_min: int,
-    y_max: int,
-) -> tuple[float, int, int, int, int, set]:
-    """Get relevant data (and do merging of repeated states if desired)"""
-
-    x_data, y_data, t_data, loops = trajectory.get_states(grid_style.x_order, grid_style.y_order)
-
-    # Get min and max values
-    # todo :: this is probably silly...
-    temp_x_min, temp_x_max = util.calculate_min_max(x_data)
-    temp_y_min, temp_y_max = util.calculate_min_max(y_data)
-
-    if grid_style.x_min is not None:
-        x_min = grid_style.x_min
-    if grid_style.x_max is not None:
-        x_max = grid_style.x_max
-
-    if grid_style.y_min is not None:
-        y_min = grid_style.y_min
-    if grid_style.y_max is not None:
-        y_max = grid_style.y_max
-
-    return (
-        max(*(t2 - t1 for t1, t2 in zip(t_data, t_data[1:])), max_duration),
-        int(min(x_min, temp_x_min)),
-        int(min(y_min, temp_y_min)),
-        int(max(x_max, temp_x_max)),
-        int(max(y_max, temp_y_max)),
-        loops,
     )
 
 
